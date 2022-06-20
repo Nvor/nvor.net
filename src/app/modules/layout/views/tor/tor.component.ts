713429afcbe3dd45b5105b8677e652cf8efb74f7
nvor <nickbaripour@gmail.com>
2022-06-20 18:05:04 -0400

    minor static cont update

--- src/app/modules/layout/views/tor/tor.component.ts
+++ src/app/modules/layout/views/tor/tor.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tor.component.scss']
 })
 export class TorComponent implements OnInit {
+  relays: string[];
+  onions: string[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  private loadData(): void {
+    this.relays = [
+      "https://metrics.torproject.org/rs.html#details/E26635F9ED41CDA8467C2AA992D81074337602EF"
+    ];
+
+    this.onions = [
+      "wx33das87876dad97bsdla5mal2mxn2uz66od5vtzd5qozslagrfzachha3f3id.onion"
+    ];
   }
 
 }
