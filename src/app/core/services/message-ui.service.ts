2da340252594f38ada518eb73df7f7836c063935
nvor <nickbaripour@gmail.com>
2023-11-16 23:11:48 -0500

    wtf is this

--- src/app/core/services/message-ui.service.ts
+++ src/app/core/services/message-ui.service.ts
@@ -7,17 +7,13 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class MessageUiService {
   private _messages$: BehaviorSubject<string> = new BehaviorSubject<string>("");
 
-  public messages$: Observable<string> = this._messages$.asObservable();
-
   constructor() { }
 
   getMessage(): Observable<string> {
-    console.log('Msg Ui Service GET')
     return this._messages$.asObservable();
   }
 
   pushMessage(message: string): void {
-    console.log('Msg Ui Service PUSH: ' + message)
     this._messages$.next(message);
   }
 }
